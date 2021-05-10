d25020001a940a8aacd23220f0d51e8c1b462f16
Hoale <nhoxlexit@yahoo.com.vn>
2021-05-10 15:21:29 +0700

    full lần 7

--- Carbook/src/main/java/carbook/controller/TuyenXeController.java
+++ Carbook/src/main/java/carbook/controller/TuyenXeController.java
@@ -48,85 +48,82 @@ import carbook.service.PaypalServices;
 @RestController
 @RequestMapping("/api/tuyenxe")
 public class TuyenXeController {
-	
-	
-	 @SuppressWarnings("unused")
-	@Autowired 
-	  private PaypalServices paypalService;
-	  
-	  @SuppressWarnings("unused")
-	@Autowired 
-	  private APIContext apiContext1;
-	  
-	  
-	  @Autowired
-	  private EmailService emailService; 
-	  
-	  @Value("${paypal.client.app}") private String clientId;
-	  
-	  @Value("${paypal.client.secret}") private String clientSecret;
-	  
-	  @Value("${paypal.mode}") private String mode;
-	  
-	   public Map<String, String> paypalSdkConfig(){ Map<String, String>
-	  sdkConfig = new HashMap<>(); sdkConfig.put("mode", mode); return sdkConfig; }
-	  
-	   public OAuthTokenCredential authTokenCredential(){ return new
-	  OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig()); }
-	  
-	   
-	  public APIContext apiContext() throws PayPalRESTException{ APIContext
-		  apiContext = new APIContext(authTokenCredential().getAccessToken());
-	  	apiContext.setConfigurationMap(paypalSdkConfig()); return apiContext; }
-	
-	
-	
-	
+
+	@SuppressWarnings("unused")
+	@Autowired
+	private PaypalServices paypalService;
+
+	@SuppressWarnings("unused")
+	@Autowired
+	private APIContext apiContext1;
+
+	@Autowired
+	private EmailService emailService;
+
+	@Value("${paypal.client.app}")
+	private String clientId;
+
+	@Value("${paypal.client.secret}")
+	private String clientSecret;
+
+	@Value("${paypal.mode}")
+	private String mode;
+
+	public Map<String, String> paypalSdkConfig() {
+		Map<String, String> sdkConfig = new HashMap<>();
+		sdkConfig.put("mode", mode);
+		return sdkConfig;
+	}
+
+	public OAuthTokenCredential authTokenCredential() {
+		return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+	}
+
+	public APIContext apiContext() throws PayPalRESTException {
+		APIContext apiContext = new APIContext(authTokenCredential().getAccessToken());
+		apiContext.setConfigurationMap(paypalSdkConfig());
+		return apiContext;
+	}
 
 	@Autowired
 	private TuyenXeDao tuyenXeDao;
-	
+
 	@Autowired
 	private BenDao benDao;
-	
+
 	@Autowired
 	private XeDao xeDao;
-	
+
 	@Autowired
 	private VeDao veDao;
-	
-	
-	//Tạo tuyến xe
-	@RequestMapping(value ="/create", method = RequestMethod.POST )
-	public ResponseEntity<BaseResponse> create(
-			@RequestBody TuyenXeRequest wrapper){
+
+	// Tạo tuyến xe
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ResponseEntity<BaseResponse> create(@RequestBody TuyenXeRequest wrapper) {
 		BaseResponse response = new BaseResponse();
-		TuyenXe tuyenXe= new TuyenXe();
+		TuyenXe tuyenXe = new TuyenXe();
 		tuyenXe.setDiemDiId(wrapper.getDiemDiId());
 		tuyenXe.setDiemToiId(wrapper.getDiemToiId());
 		tuyenXe.setKhoangCach(wrapper.getKhoangCach());
 		tuyenXe.setGiaCa(wrapper.getGiaCa());
 		tuyenXe.setKhoangThoiGian(wrapper.getThoiGianChay());
-		
+
 		tuyenXeDao.create(tuyenXe);
 		response.setData(tuyenXe);
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 	}
-	
-	//Cập nhập tuyến xe
-	@RequestMapping(value ="/{id}/update", method = RequestMethod.POST )
-	public ResponseEntity<BaseResponse> update(
-			@PathVariable(name="id")int id,
-			@RequestBody TuyenXeRequest wrapper){
+
+	// Cập nhập tuyến xe
+	@RequestMapping(value = "/{id}/update", method = RequestMethod.POST)
+	public ResponseEntity<BaseResponse> update(@PathVariable(name = "id") int id, @RequestBody TuyenXeRequest wrapper) {
 		BaseResponse response = new BaseResponse();
-		TuyenXe tuyenXe =tuyenXeDao.findOne(id);
-		if(tuyenXe==null)
-		{
+		TuyenXe tuyenXe = tuyenXeDao.findOne(id);
+		if (tuyenXe == null) {
 			response.setMessageError("không tồn tại tuyến xe này =((");
-			return new ResponseEntity<BaseResponse>(response,HttpStatus.BAD_REQUEST);
-			
+			return new ResponseEntity<BaseResponse>(response, HttpStatus.BAD_REQUEST);
+
 		} else {
-			
+
 			tuyenXe.setDiemDiId(wrapper.getDiemDiId());
 			tuyenXe.setDiemToiId(wrapper.getDiemToiId());
 			tuyenXe.setKhoangCach(wrapper.getKhoangCach());
@@ -134,137 +131,149 @@ public class TuyenXeController {
 			tuyenXe.setKhoangThoiGian(wrapper.getThoiGianChay());
 			tuyenXeDao.update(tuyenXe);
 			response.setData(tuyenXe);
-			return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+			return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 		}
 	}
-	
-	
-	//Lấy những tuyến xe khi đã có điểm đi ,điểm đến
-	@RequestMapping(value ="/get-tuyen-xe", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/get-tuyen-xe-by-id/{id}", method = RequestMethod.GET)
+	public ResponseEntity<BaseResponse> sptuyenxe(@PathVariable(name = "id") int id) {
+		BaseResponse response = new BaseResponse();
+		TuyenXe tuyenXe = tuyenXeDao.findOne(id);
+		if (tuyenXe == null) {
+			response.setMessageError("Không tìm thấy tuyến xe phù hợp");
+			return new ResponseEntity<BaseResponse>(response, HttpStatus.BAD_REQUEST);
+		} else {
+			response.setData(tuyenXe);
+			return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
+
+		}
+	}
+
+	// Lấy những tuyến xe khi đã có điểm đi ,điểm đến
+	@RequestMapping(value = "/get-tuyen-xe", method = RequestMethod.GET)
 	public ResponseEntity<BaseResponse> sptuyenxe(
 			@RequestParam(name = "diem_di_id", required = false, defaultValue = "f") int diemDiId,
 			@RequestParam(name = "diem_toi_id", required = false, defaultValue = "7") int diemToiId) {
-		BaseResponse response= new BaseResponse();
+		BaseResponse response = new BaseResponse();
 		TuyenXe tuyenXe = tuyenXeDao.spGetByDiemDiDiemToi(diemDiId, diemToiId);
-		if(tuyenXe!=null) {
-		Ben ben = benDao.findOne(diemToiId);
-		TuyenXeCustomerResponse responsedate = new TuyenXeCustomerResponse(ben,tuyenXe);
-		response.setData(responsedate);
-		}else {
+		if (tuyenXe != null) {
+			Ben ben = benDao.findOne(diemToiId);
+			TuyenXeCustomerResponse responsedate = new TuyenXeCustomerResponse(ben, tuyenXe);
+			response.setData(responsedate);
+		} else {
 			response.setMessageError("Không tìm thấy tuyến xe phù hợp");
 		}
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 	}
-	
-	//Lấy danh sách giờ chạy của 1 tuyến xe
-	@RequestMapping(value ="/{id}/gio-chay", method = RequestMethod.GET )
-	public ResponseEntity<BaseResponse> spgetAll(
-			@PathVariable(name= "id") int id,
-			@RequestParam(name="ngay",defaultValue = "01/01/2021")String ngay){
+
+	// Lấy danh sách giờ chạy của 1 tuyến xe
+	@RequestMapping(value = "/{id}/gio-chay", method = RequestMethod.GET)
+	public ResponseEntity<BaseResponse> spgetAll(@PathVariable(name = "id") int id,
+			@RequestParam(name = "ngay", defaultValue = "01/01/2021") String ngay) {
 		BaseResponse response = new BaseResponse();
-		List<Xe> list = xeDao.getXeByTuyenXe(id,ngay);
+		List<Xe> list = xeDao.getXeByTuyenXe(id, ngay);
 		List<GioChayResponse> listData = new GioChayResponse().mapToList(list);
 		response.setData(listData);
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 	}
-	
+
 	// lấy tất cả tuyến xe không lọc
-	@RequestMapping(value ="/", method = RequestMethod.GET )
-	public ResponseEntity<BaseResponse> getAll(){
-		
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<BaseResponse> getAll() {
+
 		BaseResponse response = new BaseResponse();
 		List<TuyenXeModelData> list = tuyenXeDao.findAll();
 		response.setData(list);
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 	}
-	
-	//Lấy danh sách những tuyến xe phổ biến
-	@RequestMapping(value ="/get-tuyen-xe-pho-bien", method = RequestMethod.GET )
-	public ResponseEntity<BaseResponse> spGetListTuyenXePhoBien(){
-		
+
+	// Lấy danh sách những tuyến xe phổ biến
+	@RequestMapping(value = "/get-tuyen-xe-pho-bien", method = RequestMethod.GET)
+	public ResponseEntity<BaseResponse> spGetListTuyenXePhoBien() {
+
 		BaseResponse response = new BaseResponse();
 		List<TuyenXePhoBienDataModel> list = tuyenXeDao.spGetListTuyenXePhoBien();
 		response.setData(list);
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 	}
-	
-	//Lấy số lượng vé chạy trọng ngày theo từng tuyến xe
-	@RequestMapping(value ="/list-tuyen-xe-theo-ve", method = RequestMethod.GET )
-	public ResponseEntity<BaseResponse> spGetTuyenXeTrongNgay(
-			 ){
+
+	// Lấy số lượng vé chạy trọng ngày theo từng tuyến xe
+	@RequestMapping(value = "/list-tuyen-xe-theo-ve", method = RequestMethod.GET)
+	public ResponseEntity<BaseResponse> spGetTuyenXeTrongNgay() {
 		BaseResponse response = new BaseResponse();
-		List<VeOverviewDataModel> list= veDao.spGetTuyenXeTrongNgay();
+		List<VeOverviewDataModel> list = veDao.spGetTuyenXeTrongNgay();
 		response.setData(list);
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
-	
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
+
 	}
-	
-	//Mở khóa tuyến xe cho hoạt động trở lại bình thường 
-	@RequestMapping(value ="/{id}/bat-tuyen-xe", method = RequestMethod.POST )
-	public ResponseEntity<BaseResponse> spBatTuyeXe(
-			@PathVariable(name="id")int idTuyenXe
-			 ){
+
+	// Mở khóa tuyến xe cho hoạt động trở lại bình thường
+	@RequestMapping(value = "/{id}/bat-tuyen-xe", method = RequestMethod.POST)
+	public ResponseEntity<BaseResponse> spBatTuyeXe(@PathVariable(name = "id") int idTuyenXe) {
 		BaseResponse response = new BaseResponse();
-		TuyenXe tuyenXe= tuyenXeDao.findOne(idTuyenXe);
-		if(tuyenXe!=null && tuyenXe.getStatus()==0) {
+		TuyenXe tuyenXe = tuyenXeDao.findOne(idTuyenXe);
+		if (tuyenXe != null && tuyenXe.getStatus() == 0) {
 			tuyenXe.setStatus(1);
 			tuyenXeDao.update(tuyenXe);
 			response.setData(tuyenXe);
-			return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
+			return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
 		}
-		 response.setMessageError("Không tìm thấy tuyến hoặc tuyến đã hoạt động");
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
-	
+		response.setMessageError("Không tìm thấy tuyến hoặc tuyến đã hoạt động");
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
+
 	}
-	
+
 	// Tạm ngưng chạy một tuyến xe
-	@RequestMapping(value ="/tao-tuyen-off", method = RequestMethod.POST )
-	public ResponseEntity<BaseResponse> createTuyenOff(@RequestBody TuyenOffRequest wrapper
-			 ) throws PayPalRESTException{
+	@RequestMapping(value = "/tao-tuyen-off", method = RequestMethod.POST)
+	public ResponseEntity<BaseResponse> createTuyenOff(@RequestBody TuyenOffRequest wrapper)
+			throws PayPalRESTException {
 		BaseResponse response = new BaseResponse();
 		TuyenOff tuyenOff = new TuyenOff(wrapper);
-		tuyenOff= tuyenXeDao.createTuyenOff(tuyenOff);
-		
-		List<Ve> list =veDao.getVeToCancel(wrapper.getIdTuyenXe(), wrapper.getGio(), wrapper.getNgay(), 0);
-		if(list!=null) {
-			for(Ve x: list) {
+		tuyenOff = tuyenXeDao.createTuyenOff(tuyenOff);
+
+		List<Ve> list = veDao.getVeToCancel(wrapper.getIdTuyenXe(), wrapper.getGio(), wrapper.getNgay(), 0);
+		if (list != null) {
+			for (Ve x : list) {
 				Refund refund = new Refund();
 				APIContext apiContext = apiContext();
-				Integer h =veDao.spGetCountGiuong(x.getId());	
+				Integer h = veDao.spGetCountGiuong(x.getId());
 				VeForCustomerByCodeDataModelFinal ve2 = veDao.spGetVeForCustomerByCode(x.getCode());
-				Double tienDoi= (double)x.getGiaVe()/h;
-				Double moneyRefund = (tienDoi/22000)-(tienDoi/22000)*0.1+(tienDoi/22000)*0.1;
+				Double tienDoi = (double) x.getGiaVe() / h;
+				Double moneyRefund = (tienDoi / 22000) - (tienDoi / 22000) * 0.1 + (tienDoi / 22000) * 0.1;
 				moneyRefund = Math.round(moneyRefund * 100.0) / 100.0;
-				Double moneyRefundmail = tienDoi-tienDoi*0.1+(tienDoi/22000)*0.1;
+				Double moneyRefundmail = tienDoi - tienDoi * 0.1 + (tienDoi / 22000) * 0.1;
 				String moneyRefundmailString = String.valueOf(moneyRefundmail);
 				String moneyRefundString = String.valueOf(moneyRefund);
 				Amount amount = new Amount();
 				amount.setTotal(moneyRefundString);
 				amount.setCurrency("USD");
 
-				refund.setAmount(amount);					
+				refund.setAmount(amount);
 				Sale sale = new Sale();
 				sale.setId(x.getPaypalId().toString());
 
 				try {
-					 sale.refund(apiContext, refund);
+					sale.refund(apiContext, refund);
 				} catch (PayPalRESTException e) {
-					 System.err.println(e.getDetails());
-					
+					System.err.println(e.getDetails());
+
 				}
 				x.setTrangThai(3);
 				veDao.update(x);
 				veDao.spDeleteGiuong(x.getId());
-				emailService.sendEmail(x.getEmail(),"HỦY VÉ","Vì một số vấn đề kỹ thuật,nhà xe đã hủy tuyến "+ve2.getTuyenXe()+"-- giờ chạy: "+ve2.getGioChay()+ "-- ngày chạy"+x.getDate()+"-- hồi lại tiền cho quý khách và đã lấy 10% giá trị, số tiền được hồi lại là"+ moneyRefundmailString +" vnd"+" Chân thành xin lỗi quý khách rất nhiều");
-				
+				emailService.sendEmail(x.getEmail(), "HỦY VÉ",
+						"Vì một số vấn đề kỹ thuật,nhà xe đã hủy tuyến " + ve2.getTuyenXe() + "-- giờ chạy: "
+								+ ve2.getGioChay() + "-- ngày chạy" + x.getDate()
+								+ "-- hồi lại tiền cho quý khách và đã lấy 10% giá trị, số tiền được hồi lại là"
+								+ moneyRefundmailString + " vnd" + " Chân thành xin lỗi quý khách rất nhiều");
+
 			}
-		} 
+		}
 
 		response.setMessageError("Thành công rồi nè");
 		response.setData(wrapper);
-		return new ResponseEntity<BaseResponse>(response,HttpStatus.OK);
-		
+		return new ResponseEntity<BaseResponse>(response, HttpStatus.OK);
+
 	}
-	
 
 }
