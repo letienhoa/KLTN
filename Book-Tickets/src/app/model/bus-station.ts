14d67c4c79b0c71546cf8b1f3824041fc85f0bc2
Hoale <nhoxlexit@yahoo.com.vn>
2021-07-21 18:24:59 +0700

    gg
    gg

--- Book-Tickets/src/app/model/bus-station.ts
+++
@@ -1,8 +0,0 @@
-export interface BusStation{
-    id:Int16Array;
-    ben_toi:string;
-    dia_chi:string;
-    thanh_pho:string;
-    picture:string;
-}
-
--- Book-Tickets/src/app/model/bus.ts
+++
@@ -1,9 +0,0 @@
-export interface Bus{
-    ten_xe:string;
-    hang_xe:string;
-    tuyen_san_sang_id:string;
-    tuyen_off_id:string;
-    gio_chay:string;
-    diem_di_id: string;
-    diem_toi_id: string;
-}
